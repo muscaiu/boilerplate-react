a74d61c536389e8fe31aeb25354edb71a9b055df
Cristian Muscalu <muscaiu@yahoo.com>
2018-06-08 18:47:26 +0300

    using redux for test

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import { action } from '../actions/action';
+
 class App extends Component {
+
+  simpleAction = (event) => {
+    this.props.simpleAction();
+  }
+
   render() {
     return (
-      <div className="App">
-        App
+      <div>
+        <pre>
+          {
+            JSON.stringify(this.props)
+          }
+        </pre>
+        <button onClick={this.simpleAction}>Test redux action</button>
       </div>
     );
   }
 }
 
-export default connect()(App);
+const mapStateToProps = state => ({
+  ...state
+})
+
+const mapDispatchToProps = dispatch => ({
+  simpleAction: () => dispatch(action())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
