2b8aad81453f094a7dd372be2a47b4e09b081b9d
Cristian Muscalu <muscaiu@yahoo.com>
2018-06-08 18:24:50 +0300

    added absolute paths

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -11,6 +11,7 @@ const eslintFormatter = require('react-dev-utils/eslintFormatter');
 const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
 const getClientEnvironment = require('./env');
 const paths = require('./paths');
+const SRC_PATH = path.resolve(__dirname, '../src');
 
 // Webpack uses `publicPath` to determine where the app is being served from.
 // In development, we always serve from the root. This makes config easier.
@@ -88,6 +89,7 @@ module.exports = {
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
+      'components': path.resolve(SRC_PATH, 'components'),
     },
     plugins: [
       // Prevents users from importing files from outside of src/ (or node_modules/).
